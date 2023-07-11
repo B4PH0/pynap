ec866d8eefca17df9f6d9d6ffc6bc8c881801f82
B4PH0 <134951170+B4PH0@users.noreply.github.com>
2023-07-11 10:42:41 -0300

    scan tcp corrigido

---
+++ utils/UdpThreading.py
@@ -0,0 +1,6 @@
+import socket
+import threading
+from args import *
+
+def scan_ports(host, port):
+    
--- utils/args.py
+++ utils/args.py
@@ -3,6 +3,13 @@ import argparse
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('ip')
-    parser.add_argument('-sT')
+    parser.add_argument('-sT', action='store_true')
+    global args
     args = parser.parse_args()
     return args.ip
+
+def validar_st():
+    if args.sT:
+        return True
+    else:
+        return False
